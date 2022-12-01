2028c17b62e9942c58a02a41e6f9a329d2aab1bb
Van Thieu <thieudvts2208034@fpt.edu.vn>
2022-12-01 19:56:12 +0700

    all

--- scripts.js
+++ scripts.js
@@ -20,19 +20,30 @@ function topFunction() {
 }
 
 
+// Log in form
 
-function submitLogin() {
-    var loginEmail = document.getElementById("loginName").value;
-    var password = document.getElementById("loginPassword").value;
-    // if(!loginEmail[0].value) {
-    //   return;
-    // }
+function validateonSubmit() {
+  let self = this; // setup calls to the "this" values of the class described in the constructor
 
-    // if(!password[0].value) {
-    //   return;
-    // }
-    // document.getElementById("frmLogin").frmLogin.submit();
-    alert("Thanh cong");
-    
+  // add a "submit" event listener to the form
+  this.form.addEventListener("submit", (e) => {
+      // remove default functionality 
+      e.preventDefault();
+      var error = 0;
+      // loop through the fields and check them against a function for validation
+      self.fields.forEach((field) => {
+          const input = document.querySelector(`#${field}`);
+          if (self.validateFields(input) == false) {
+              // if a field does not validate, auto-increment our error integer
+              error++;
+          }
+      });
+      // if everything validates, error will be 0 and can continue
+      if (error == 0) {
+          //do login api here or in this case, just submit the form and set a localStorage item
+          localStorage.setItem("auth", 1);
+          this.form.submit();
+      }
+  });
 }
 
